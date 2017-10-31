40686769bbdf68efad1e798a6a7b8f6544697d90
GemApple <gemmarrgemma@gmail.com>
2017-10-31 02:03:16 +0000

    Update Feather.ts

--- Feather.ts
+++ Feather.ts
@@ -1,5 +1,15 @@
-const magik = magikcraft.io;
-
-function Feather() {
-	magik.declaro('feather')
+/give @p potion 1 16392 {
+    CustomPotionEffects:[
+}
+    Id: 8,
+    Amplifier: 99999
+    Duration: 1999980
+    }
+ ],
+display: {
+    Name: "Jump Power!"
+    Lore: [
+        "Make you fly"
+    ]
+}
 }
